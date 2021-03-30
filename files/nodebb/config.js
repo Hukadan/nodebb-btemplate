8db390933c8b9cb69aed7a92e509ce770b5ccafe
Hukadan <hukadan@desktop.home.hukadan.org>
2021-03-30 20:33:46 +0200

    try setup without user input

---
+++ files/nodebb/config.js
@@ -0,0 +1,14 @@
+{
+    "url": "${NODEBB_URL}:${NODEBB_PORT}",
+    "secret": "${NODEBB_SECRET}",
+    "database": "mongo",
+    "mongo": {
+        "host": "127.0.0.1",
+        "port": "27017",
+        "username": "${DB_USER}",
+        "password": "${DB_PASSWORD}",
+        "database": "${DB_NAME}",
+        "uri": ""
+    },
+    "port": "${NODEBB_PORT}"
+}

3fec265403e98c4c0caea5394575419acf70af64
Hukadan <hukadan@desktop.home.hukadan.org>
2021-03-30 08:49:08 +0200

    typo and start nodebb at the end.:wq

--- root/mongo-config.js
+++ root/mongo-config.js
@@ -1,3 +1,3 @@
 use ${DB_NAME}
-db.createUser({user: "${DB_USER}", pwd: "{DB_PASSWORD}", roles: ["readWrite"]})
+db.createUser({user: "${DB_USER}", pwd: "${DB_PASSWORD}", roles: ["readWrite"]})
 db.grantRolesToUser("${DB_USER}",[{ role: "clusterMonitor", db: "admin" }])
